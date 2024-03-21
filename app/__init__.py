1146b437c23c05ae37f9437d7857fb2080d48c75
jeffch19 <17jcummings@gmail.com>
2024-03-21 16:37:46 -0400

    added to dashboard and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
+from app.routes import home # Import the home blueprint
 
 def create_app(test_config=None):
-  # set up app config
-  app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
+    # set up app config
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
 
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
+    # Register the home blueprint
+    app.register_blueprint(home)
 
-  return app
+    return app
