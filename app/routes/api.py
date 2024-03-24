0f5c5af0ed4434599a07d80e72b6f0796e37935a
jeffch19 <17jcummings@gmail.com>
2024-03-23 20:18:03 -0400

    added api to homepage

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
 import sys
+from app.models import User, Post, Comment, Vote
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -56,3 +57,47 @@ def login():
     session['user_id'] = user.id
     session['loggedIn'] = True
     return jsonify(id=user.id)
+
+@bp.route('/comments', methods=['POST'])
+def comment():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # create a new comment
+        newComment = Comment(
+            comment_text=data['comment_text'],
+            post_id=data['post_id'],
+            user_id=session.get('user_id')
+        )
+
+        db.add(newComment)
+        db.commit()
+        return jsonify(id=newComment.id) # Moved inside the try block
+    except Exception as e: # Catch specific exceptions
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message='Comment failed'), 500
+    
+
+@bp.route('/posts/upvote', methods=['PUT'])
+def upvote():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new vote with incoming id and session id
+    newVote = Vote(
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newVote)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Upvote failed'), 500
+
+  return '', 204
